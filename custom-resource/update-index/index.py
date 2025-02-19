728a5b9693dd6c96a4748846b131336257845f4a
Shunsuke Goto <gotosh@amazon.co.jp>
2025-02-19 11:32:12 +0900

    first commit

---
+++ custom-resource/update-index/index.py
@@ -0,0 +1,41 @@
+from opensearchpy import OpenSearch, RequestsHttpConnection, AWSV4SignerAuth
+import boto3
+import json
+import os
+
+
+def handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+
+    host = os.environ['OPENSEARCH_HOST'].replace('https://', '')
+    region = os.environ['AWS_REGION']
+    service = 'aoss'
+    credentials = boto3.Session().get_credentials()
+    auth = AWSV4SignerAuth(credentials, region, service)
+
+    client = OpenSearch(
+        hosts=[{'host': host, 'port': 443}],
+        http_auth=auth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection
+    )
+
+    index_name = event['ResourceProperties']['IndexName']
+    index_body = json.loads(event['ResourceProperties']['IndexBody'])
+
+    if event['RequestType'] == 'Create':
+        response = client.indices.create(index=index_name, body=index_body)
+        print("Index created: " + json.dumps(response, indent=2))
+        return {"index_name": index_name}
+
+    elif event['RequestType'] == 'Delete':
+        response = client.indices.delete(index=index_name)
+        print("Index deleted: " + json.dumps(response, indent=2))
+        return {}
+
+    elif event['RequestType'] == 'Update':
+        response = client.indices.put_mapping(
+            index=index_name, body=index_body["mappings"])
+        print("Index updated: " + json.dumps(response, indent=2))
+        return {}
--- lib/aoss-cdk-index-stack.ts
+++ lib/aoss-cdk-index-stack.ts
@@ -1,16 +1,128 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { PythonFunction } from "@aws-cdk/aws-lambda-python-alpha";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import {
+  CfnAccessPolicy,
+  CfnCollection,
+  CfnSecurityPolicy,
+} from "aws-cdk-lib/aws-opensearchserverless";
+import { Provider } from "aws-cdk-lib/custom-resources";
 
 export class AossCdkIndexStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const collection = new CfnCollection(this, "OpenSearchCollection", {
+      name: "my-opensearch-collection",
+      type: "SEARCH",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AossCdkIndexQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const updateIndexFunction = new PythonFunction(
+      this,
+      "UpdateIndexFunction",
+      {
+        entry: "custom-resource/update-index",
+        runtime: Runtime.PYTHON_3_12,
+        initialPolicy: [
+          new PolicyStatement({
+            actions: ["aoss:*"],
+            resources: ["*"],
+          }),
+        ],
+        timeout: cdk.Duration.minutes(15),
+        environment: {
+          OPENSEARCH_HOST: collection.attrCollectionEndpoint,
+        },
+      }
+    );
+
+    const collectionEncryptionPolicy = new CfnSecurityPolicy(
+      this,
+      "CollectionEncryptionPolicy",
+      {
+        name: "encryption-policy",
+        type: "encryption",
+        policy: JSON.stringify({
+          Rules: [
+            {
+              ResourceType: "collection",
+              Resource: [`collection/${collection.name}`],
+            },
+          ],
+          AWSOwnedKey: true,
+        }),
+      }
+    );
+
+    collection.addDependency(collectionEncryptionPolicy);
+
+    new CfnSecurityPolicy(this, "CollectionNetworkPolicy", {
+      name: "network-policy",
+      type: "network",
+      policy: JSON.stringify([
+        {
+          Rules: [
+            {
+              ResourceType: "collection",
+              Resource: [`collection/${collection.name}`],
+            },
+            {
+              ResourceType: "dashboard",
+              Resource: [`collection/${collection.name}`],
+            },
+          ],
+          AllowFromPublic: true,
+        },
+      ]),
+    });
+
+    new CfnAccessPolicy(this, "CollectionAccessPolicy", {
+      name: "access-policy",
+      type: "data",
+      policy: JSON.stringify([
+        {
+          Rules: [
+            {
+              ResourceType: "collection",
+              Resource: [`collection/${collection.name}`],
+              Permission: ["aoss:*"],
+            },
+            {
+              ResourceType: "index",
+              Resource: [`index/${collection.name}/*`],
+              Permission: ["aoss:*"],
+            },
+          ],
+          Principal: [
+            `arn:aws:iam::${cdk.Stack.of(this).account}:role/Admin`,
+            updateIndexFunction.role!.roleArn,
+          ],
+        },
+      ]),
+    });
+
+    const updateIndexProvider = new Provider(this, "UpdateIndexProvider", {
+      onEventHandler: updateIndexFunction,
+    });
+
+    const indexDefinition = {
+      mappings: {
+        properties: {
+          title: { type: "text" },
+          content: { type: "text" },
+          // timestamp: { type: "date" }, // コメントアウト外して index update を確認
+        },
+      },
+    };
+
+    new cdk.CustomResource(this, "UpdateIndexResource", {
+      serviceToken: updateIndexProvider.serviceToken,
+      properties: {
+        IndexName: "my-index",
+        IndexBody: JSON.stringify(indexDefinition),
+      },
+    });
   }
 }
